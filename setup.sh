a932d53f26963780ef1cb563ffd8b5fbd9c41362
7054company <34761088+7054company@users.noreply.github.com>
2019-11-18 14:53:12 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+echo "Installing..BigToken"
+cd $HOME
+sudo apt install figlet
+apt install figlet
+figlet 7054company
+chmod 777 bot.php 
+sudo apt install php
+apt install php
+echo "Running-setup"
+echo "setup-complete"
+Now type php bot.php
